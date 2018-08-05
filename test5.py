79149bfc230bc7451a3b8b4a8639e4a21d0d3e1b
Natalie Gorodnicheva <nataligorod@yandex.ru>
2018-08-05 12:40:20 +0300

    Задание 8
    (попытка № 2)

--- test5.py
+++ test5.py
@@ -4,27 +4,39 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get('http://localhost/litecart/')
-list = driver.find_elements_by_css_selector("li.product.column.shadow.hover-light")
-form = list[0]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[1]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[2]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[3]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[4]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[5]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[6]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[7]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[8]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[9]
-form.find_element_by_css_selector("[class*=sticker]")
-form = list[10]
-form.find_element_by_css_selector("[class*=sticker]")
+list = driver.find_elements_by_css_selector("div.image-wrapper")
+x = 0
+for i in list:
+    list[x].find_element_by_css_selector("[class*=sticker]")
+    x += 1
+driver.quit()
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get('http://localhost/litecart/')
+list = driver.find_elements_by_css_selector("div.image-wrapper")
+x = 0
+for i in list:
+    list[x].find_element_by_css_selector("[class*=sticker]")
+    x += 1
+driver.quit()
+
+driver = webdriver.Edge()
+driver.implicitly_wait(10)
+driver.get('http://localhost/litecart/')
+list = driver.find_elements_by_css_selector("div.image-wrapper")
+x = 0
+for i in list:
+    list[x].find_element_by_css_selector("[class*=sticker]")
+    x += 1
+driver.quit()
+
+driver = webdriver.Ie()
+driver.implicitly_wait(10)
+driver.get('http://localhost/litecart/')
+list = driver.find_elements_by_css_selector("div.image-wrapper")
+x = 0
+for i in list:
+    list[x].find_element_by_css_selector("[class*=sticker]")
+    x += 1
 driver.quit()
